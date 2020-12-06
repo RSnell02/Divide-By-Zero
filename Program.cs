cac5655d55fef509639ab593ea47e126cb6c74ad
Ryan Snell <74518190+RSnell02@users.noreply.github.com>
2020-12-06 06:53:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Project: DivideByZeroException
+ * Filename: Program.cs
+ * Author: R. Snell
+ * Purpose: To demonstrate handilng ingical exception (division by zero)
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spr2020_DivideByZeroException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the numerator
+            Console.Write("Please enter an integer for the numerator: ");
+            var numerator = int.Parse(Console.ReadLine());
+            // Get the denominator
+            Console.Write("Please enter an integer for the denominator: ");
+            var denominator = int.Parse(Console.ReadLine());
+
+            // Divide the inegers, then display the result
+            var result = numerator / denominator;
+            Console.WriteLine($"Result: {numerator}/{denominator} = " +
+                $"{result}"); 
+        }   // end Main()
+    }   // end class
+}   // end namespace
